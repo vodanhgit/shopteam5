6f106c7767c14afcc42afd5304dfd21c478f69e9
HieuuNguyenn <nguyenhieuct0308@gmail.com>
2023-05-19 22:18:23 +0700

    Push lên thử

--- shopteam5/src/main/java/com/poly/controller/SanPhamController.java
+++ shopteam5/src/main/java/com/poly/controller/SanPhamController.java
@@ -2,19 +2,18 @@ package com.poly.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-@RequestMapping("danhsach")
+@RequestMapping("sanpham")
 @Controller
-public class danhSachController {
+public class SanPhamController {
 	@Autowired
 	HttpServletRequest req;
-	@GetMapping("form")
-	public String form() {
-	return "danhSachDatHang";
+	@GetMapping("formsp")
+	public String formsp() {
+	return "sanpham";
 	}
 }
