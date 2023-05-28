783834d770f374c48108cfe70be7b2c7c90c1d81
Võ Hoàng Gia Bảo <85601824+baovo1608@users.noreply.github.com>
2023-05-29 02:16:48 +0700

    Revert "chỉnh sửa form admin"
    This reverts commit 0641b7ee3be92b47236bb1333b6e1b7d5c6a7074.

--- shopteam5/src/main/java/com/poly/controller/trangChinhController.java
+++ shopteam5/src/main/java/com/poly/controller/trangChinhController.java
@@ -13,7 +13,7 @@ public class trangChinhController {
 		HttpServletRequest req;
 		@RequestMapping("index")
 		public String index() {
-		return "accers/index";
+		return "index";
 		}
 		public String dk() {
 		return "forward:/checkout";
@@ -22,14 +22,6 @@ public class trangChinhController {
 	}
 		@RequestMapping("formadmin")
 		public String adminform() {
-		return "accers/home";
-		}
-		@RequestMapping("text")
-		public String text() {
-		return "/admin/text";
-		}
-		@RequestMapping("editprofile")
-		public String editprofile() {
-		return "/admin/editprofile";
+		return "home";
 		}
 }
