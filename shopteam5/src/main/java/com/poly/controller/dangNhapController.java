5c8637881f27a9a86a1eeb9bf09ec7c6b9061a47
AigNeyung <chaugianguyen1804@gmail.com>
2023-05-29 21:23:28 +0700

    Mới

---
+++ shopteam5/src/main/java/com/poly/controller/dangNhapController.java
@@ -0,0 +1,20 @@
+package com.poly.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@RequestMapping("dangnhap")
+@Controller
+public class dangNhapController {
+	@Autowired
+	HttpServletRequest req;
+	@GetMapping("form")
+	public String form() {
+	return "dangNhap";
+	}
+}
