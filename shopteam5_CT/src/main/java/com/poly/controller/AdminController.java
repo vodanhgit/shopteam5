dbda1ef9ff21ecfac6f82c1f2b8ca7f3e344b686
LuongVanQuoc <luongvanquoc18121990@gmail.com>
2023-07-27 23:22:03 +0700

    tao class admincontroller

---
+++ shopteam5_CT/src/main/java/com/poly/controller/AdminController.java
@@ -0,0 +1,40 @@
+package com.poly.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	@GetMapping("/admin/thongke")
+	public String thongKe(Model model) {
+		return "admin/thongke";
+	}
+	
+	@ModelAttribute("trangthai_sosanh")
+	public Map<String, String> getTrangThaiSoSanh(){
+		Map<String, String> map = new HashMap<>();
+		map.put("wait", "Chờ xét duyệt");
+		map.put("approved", "Đã xét duyệt");
+		map.put("cancel", "Xét duyệt thất bại");
+		map.put("delivery", "Đang giao hàng");
+		map.put("delivered", "Đã giao hàng");
+		return map;
+	}
+	
+	@ModelAttribute("trangthai")
+	public Map<String, String> getTrangThai(){
+		Map<String, String> map = new HashMap<>();
+		map.put("wait", "Chờ xét duyệt");
+		map.put("approved", "Đã xét duyệt");
+		map.put("cancel", "Xét duyệt thất bại");
+		map.put("delivery", "Đang giao hàng");
+		map.put("delivered", "Đã giao hàng");
+		return map;
+	}
+}
