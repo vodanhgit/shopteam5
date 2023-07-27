6f3c6ff85212fd2d3cd9d4963e3762a1642e7a5f
MaiTruongThanh <118740990+MaiTruongThanh@users.noreply.github.com>
2023-07-27 22:41:39 +0700

    com.poly

---
+++ shopteam5_CT/src/main/java/com/poly/Java6Lab3Application.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java6Lab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java6Lab3Application.class, args);
+	}
+
+}
