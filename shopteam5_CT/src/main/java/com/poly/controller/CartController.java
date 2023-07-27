839ba1741f260f9b1aa1daeb0a345b6a379af7bf
MaiTruongThanh <118740990+MaiTruongThanh@users.noreply.github.com>
2023-07-27 22:36:17 +0700

    hoàn thành giao diện cart

---
+++ shopteam5_CT/src/main/java/com/poly/controller/CartController.java
@@ -0,0 +1,13 @@
+package com.poly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CartController {
+	@GetMapping("/home/cart")
+	public String index(Model model) {
+		return "cart/cart";
+	}
+}
