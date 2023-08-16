9d98c68ca5b6cd601c32fc255b46ccd6b3b043a2
LuongVanQuoc <113768193+LuongVanQuoc@users.noreply.github.com>
2023-08-16 13:03:57 +0700

    Thay đổi phần thống kê, thêm phần quên mật khẩu

--- shopteam5_CT/src/main/java/com/poly/controller/AccountController.java
+++ shopteam5_CT/src/main/java/com/poly/controller/AccountController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,18 +21,19 @@ import com.poly.entities.TaiKhoan;
 import com.poly.reponstory.GioHangDao;
 import com.poly.reponstory.TaiKhoanDao;
 import com.poly.service.CookieService;
+import com.poly.service.MailerServiceImpl;
 import com.poly.service.ParamService;
 import com.poly.service.SessionService;
 import com.poly.service.SessionService_quenMatKhau;
 
 @Controller
 public class AccountController {
-//	@Autowired
-//	MailerServiceImpl mailer;
-	
+	@Autowired
+	MailerServiceImpl mailer;
+
 	@Autowired
 	HttpSession session;
-	
+
 	@Autowired
 	SessionService sessionService;
 
@@ -148,61 +150,169 @@ public class AccountController {
 		return "home/forgot-password";
 	}
 
-	@GetMapping("/home/check-mail")
-//	Model model, @PathVariable("email") String email
-	public String check_mail() {
-//		sessionService_quenmatkhau.set("maxacnhan_email", randomString);
-//		String styledIcon1 = icon1.replace("\n", "<br>").replace(" ", "&nbsp;")
-//				.replace("<h>", "<h3 style=\"color: blue; text-decoration: underline;\">").replace("</h>", "</h3>");
-//		model.addAttribute("email", email);
-//		String content = "<html><body>" + styledIcon1 + "</body></html>";
-//		mailer.send(email, Subject, content);
-		return "home/check-mail";
+	public static String generateRandomString(Integer length) {
+		if (length == null) {
+			throw new IllegalArgumentException("Length must not be null");
+		}
+		String characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		Random random = new Random();
+		StringBuilder sb = new StringBuilder(length);
+
+		for (int i = 0; i < length; i++) {
+			int index = random.nextInt(characters.length());
+			char randomChar = characters.charAt(index);
+			sb.append(randomChar);
+		}
+
+		return sb.toString();
 	}
 
+	String randomString = generateRandomString(10);
+	String Subject = "███ SHOPTEAM5 XIN CHÀO, ĐÂY LÀ MÃ XÁC NHẬN CỦA BẠN ███";
+	String icon1 = "" + "Mã xác nhận email của bạn là: <h>" + randomString + "</h>\n"
+			+ "████████████████████████████████████████\n" + "████████████████████████████████████████\n"
+			+ "██████▀░░░░░░░░▀████████▀▀░░░░░░░▀██████\n" + "████▀░░░░░░░░░░░░▀████▀░░░░░░░░░░░░▀████\n"
+			+ "██▀░░░░░░░░░░░░░░░░▀▀░░░░░░░░░░░░░░░░▀██\n" + "██░░░░░░░░░░░░░░░░░░░▄▄░░░░░░░░░░░░░░░██\n"
+			+ "██░░░░░░░░░░░░░░░░░░█░█░░░░░░░░░░░░░░░██\n" + "██░░░░░░░░░░░░░░░░░▄▀░█░░░░░░░░░░░░░░░██\n"
+			+ "██░░░░░░░░░░████▄▄▄▀░░▀▀▀▀▄░░░░░░░░░░░██\n" + "██▄░░░░░░░░░████░░░░░░░░░░█░░░░░░░░░░▄██\n"
+			+ "████▄░░░░░░░████" + randomString + "░█░░░░░░░░▄████\n" + "██████▄░░░░░████▄▄▄░░░░░░░█░░░░░░▄██████\n"
+			+ "████████▄░░░▀▀▀▀░░░▀▀▀▀▀▀▀░░░░░▄████████\n" + "██████████▄░░░░░░░░░░░░░░░░░░▄██████████\n"
+			+ "████████████▄░░░░░░░░░░░░░░▄████████████\n" + "██████████████▄░░░░░░░░░░▄██████████████\n"
+			+ "████████████████▄░░░░░░▄████████████████\n" + "██████████████████▄▄▄▄██████████████████\n"
+			+ "████████████████████████████████████████\n" + "████████████████████████████████████████\n";
+
+	@RequestMapping("/home/check-mail")
+	public String check_mail(Model model, @RequestParam("email") String email) {
+		if (email.equals("")) {
+			model.addAttribute("kiemtramail", "<<<  Vui lòng nhập email  >>>");
+			return "quenMatKhau";
+		} else {
+			TaiKhoan kiemtramail = taiKhoanDao.findByEmail(email);
+			if (kiemtramail == null) {
+				model.addAttribute("kiemtramail", "<<<  Email không tồn tại  >>>");
+				return "home/forgot-password";
+			} else {
+				model.addAttribute("email", email);
+				return "redirect:/home/send-email/" + email;
+			}
+		}
+	}
+	
+	@RequestMapping("/home/send-email/{email}")
+	public String sendemail(Model model, @PathVariable("email") String email) {
+		sessionService_quenmatkhau.set("maxacnhan_email", randomString);
+		String styledIcon1 = icon1.replace("\n", "<br>")
+                .replace(" ", "&nbsp;")
+                .replace("<h>", "<h3 style=\"color: blue; text-decoration: underline;\">")
+                .replace("</h>", "</h3>");
+		model.addAttribute("email", email);
+		String content = "<html><body>" + styledIcon1 + "</body></html>";
+		mailer.send(email, Subject, content);
+		System.out.println(email +'1');
+		return "home/check-mail";
+	}
+	
+	String emailthaydoimatkhau = "";
+	@RequestMapping("/home/check-code/{email}")
+	public String xacnhanma(Model model,@RequestParam("email") String email, @RequestParam("maxacnhan") String maxacnhan) {
+		if (maxacnhan.equals("")) {
+			model.addAttribute("kiemtramaxacnhan","<<<  Vui lòng nhập mã xác nhận!  >>>");
+			model.addAttribute("email", email);
+			return "home/check-mail";
+		}
+		else if (!maxacnhan.equals( sessionService_quenmatkhau.get("maxacnhan_email"))) {
+			model.addAttribute("kiemtramaxacnhan","<<<  Mã xác nhận không hợp lệ!  >>>");
+			model.addAttribute("email", email);
+			return "home/check-mail";
+		}
+		System.out.println(email);
+		emailthaydoimatkhau = email.toString();
+		return "redirect:/home/set-new-password";
+	}
+
+	String thongbaothaydoimatkhau_tendangnhap="";
+	String thongbaothaydoimatkhau_matkhau="";
+	String tendn;
+	String matk;
 	@GetMapping("/home/set-new-password")
-	public String set_new_password() {
+	public String set_new_password(Model model) {
+		model.addAttribute("email",emailthaydoimatkhau);
+		System.out.println(emailthaydoimatkhau);
+		model.addAttribute("thongbaothaydoimatkhau_tendangnhap", thongbaothaydoimatkhau_tendangnhap);
+		model.addAttribute("thongbaothaydoimatkhau_matkhau", thongbaothaydoimatkhau_matkhau);
+		model.addAttribute("tendn", tendn);
+		model.addAttribute("matk", matk);
 		return "home/set-new-password";
+	}
+	
+	@RequestMapping("/home/fg-chane-password/{email}")
+	public String xacnhanthaydoimatkhau(Model model,@RequestParam("email") String email, 
+														@RequestParam("tendangnhap") String tenDangNhap, 
+															@RequestParam("matkhau") String matkhau, 
+																@RequestParam("xacnhanmatkhau") String xacnhanmatkhau) {
+		TaiKhoan kiemtrataikhoan_email = taiKhoanDao.findByEmail(email);
+		tendn = tenDangNhap;
+		matk = matkhau;
+		
+		if(kiemtrataikhoan_email == null) {
+			model.addAttribute("message", "Không tìm thấy tài khoản có mail tương tự!");
+			return "home/set-new-password";
+		}
+		
+		if (!tenDangNhap.equals(kiemtrataikhoan_email.getTenTaiKhoan())) {
+			thongbaothaydoimatkhau_tendangnhap = "Tên đăng nhập không hợp lệ!";
+			return "redirect:/home/set-new-password";
+		}
+		else if(!xacnhanmatkhau.equals(matkhau)) {
+			thongbaothaydoimatkhau_tendangnhap="";
+			thongbaothaydoimatkhau_matkhau = "Xác nhận mật khẩu không hợp lệ!";
+			return "redirect:/home/set-new-password";
+		}
+		else {
+			kiemtrataikhoan_email.setMatKhau(matkhau);
+			taiKhoanDao.save(kiemtrataikhoan_email);
+			model.addAttribute("message", "Bạn đã đổi mật khẩu thành công! Vui lòng đăng nhập!");
+			return "home/login";
+		}
 	}
 
 	@GetMapping("/home/chane-password")
 	public String chane_password() {
 		return "home/chane-password";
 	}
-	
+
 	@PostMapping("/home/chanepassword")
-	public String dmk( Model model, HttpServletRequest req) {
+	public String dmk(Model model, HttpServletRequest req) {
 		String un = paramService.getString("username", "");
 		String pw = paramService.getString("password", "");
 		String pwm = paramService.getString("passwordm", "");
 		String pwm1 = paramService.getString("passwordm1", "");
-		if(un.equals("")||pw.equals("")){
+		if (un.equals("") || pw.equals("")) {
 			model.addAttribute("message", "Vui lòng nhập đầy đủ thông tin!");
-		}else{
-			  if (!pwm.equals(pwm1)) {
-		            model.addAttribute("message", "Xác nhận mật khẩu mới không khớp.");
-		            return "home/chane-password";
-		        }
+		} else {
+			if (!pwm.equals(pwm1)) {
+				model.addAttribute("message", "Xác nhận mật khẩu mới không khớp.");
+				return "home/chane-password";
+			}
 
-		        // Lấy thông tin tài khoản từ cơ sở dữ liệu
-		        TaiKhoan taiKhoan = taiKhoanDao.findByTenTaiKhoanThongThuong(un);
+			// Lấy thông tin tài khoản từ cơ sở dữ liệu
+			TaiKhoan taiKhoan = taiKhoanDao.findByTenTaiKhoanThongThuong(un);
 
-		        // Kiểm tra mật khẩu cũ
-		        if (!taiKhoan.getMatKhau().equals(pw)) {
-		            model.addAttribute("message", "Mật khẩu cũ không đúng.");
-		            return "home/chane-password";
-		        }
+			// Kiểm tra mật khẩu cũ
+			if (!taiKhoan.getMatKhau().equals(pw)) {
+				model.addAttribute("message", "Mật khẩu cũ không đúng.");
+				return "home/chane-password";
+			}
 
-		        // Cập nhật mật khẩu mới
-		        taiKhoan.setMatKhau(pwm);
-		        taiKhoanDao.save(taiKhoan);
+			// Cập nhật mật khẩu mới
+			taiKhoan.setMatKhau(pwm);
+			taiKhoanDao.save(taiKhoan);
 
-		        // Hiển thị thông báo thành công
-		        model.addAttribute("message", "Đổi mật khẩu thành công.");
+			// Hiển thị thông báo thành công
+			model.addAttribute("message", "Đổi mật khẩu thành công.");
 
-		        return "home/chane-password";
-		    }
-	
+			return "home/chane-password";
+		}
 
 		return "home/chane-password";
 	}
--- shopteam5_CT/src/main/java/com/poly/controller/AdminController.java
+++ shopteam5_CT/src/main/java/com/poly/controller/AdminController.java
@@ -6,15 +6,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -46,7 +50,7 @@ public class AdminController {
 
 	@Autowired
 	ServletContext app;
-	
+
 	@Autowired
 	SessionService session;
 
@@ -65,28 +69,23 @@ public class AdminController {
 			model.addAttribute("huy", null);
 			model.addAttribute("ten", "readonly");
 		}
+		//Kiểm tra tài khoản đang đăng nhập có phải admin hay không
+		String account = session.get(SessionAttr.CURRENT_USER);
+
+		TaiKhoan chkAccount = taikhoandao.findByTenTaiKhoanThongThuong(account);
+
+		if (!chkAccount.isQuyen()) {
+			return "home/error404";
+		}
+
 		String check = session.get(SessionAttr.Admin);
 		System.out.println(check);
 		TaiKhoan tk = taikhoandao.findByTenTaiKhoanThongThuong(check);
 		model.addAttribute("thongtintaikhoan", tk);
 
-//		TaiKhoan item = new TaiKhoan();
-//		model.addAttribute("item", item);
-//		List<TaiKhoan> items = taikhoandao.findAll();
-//
-//		model.addAttribute("items", items);
-//		String untk = check;
-//		if (tk.isQuyen() == true) {
-//			req.getSession().setAttribute("quanly", untk);
-//			req.getSession().setAttribute("user", null);
-//		} else {
-//			req.getSession().setAttribute("quanly", null);
-//			req.getSession().setAttribute("user", untk);
-//		}
-
 		return "admin/index";
 	}
-	
+
 	@RequestMapping("/admin/update-information")
 	public String capnhap(@RequestParam("hoTenNguoiDung") String hoTenNguoiDung, @RequestParam("email") String email,
 			@RequestParam("soDienThoai") String soDienThoai, @RequestParam("gioiTinh") Boolean gioiTinh,
@@ -117,35 +116,34 @@ public class AdminController {
 		}
 		taikhoandao.save(taikhoan);
 		ma = 0;
-		
+
 		return "redirect:/admin/index";
 	}
 
 	@GetMapping("/admin/thongke")
 	public String thongKe(Model model) {
+		String account = session.get(SessionAttr.CURRENT_USER);
+
+		TaiKhoan chkAccount = taikhoandao.findByTenTaiKhoanThongThuong(account);
+
+		if (!chkAccount.isQuyen()) {
+			return "home/error404";
+		}
 		return "admin/thongke";
 	}
 
-	@RequestMapping("thongke")
-	public String thongke(Model model) {
-	    model.addAttribute("chontrangthai", "Chọn trạng thái");
-	    model.addAttribute("chontrangthai_sosanh", "Chọn trạng thái");
-	    return "admin/thongke";
-	}
-
 	@RequestMapping("/admin/thongkedoanhthu")
-	public String thongkedoanhthu(Model model,
-	        @RequestParam("tungay") String tungay, @RequestParam("denngay") String denngay,
-	        @RequestParam("trangthai") String trangthai, @RequestParam("tungay_sosanh") String tungay_sosanh,
-	        @RequestParam("denngay_sosanh") String denngay_sosanh, @RequestParam("trangthai_sosanh") String trangthai_sosanh) {
+	public String thongkedoanhthu(Model model, @RequestParam("tungay") String tungay,
+			@RequestParam("denngay") String denngay, @RequestParam("trangthai") String trangthai) {
 
-	    // Các phần code xử lý doanh thu và các thuộc tính khác tôi giữ nguyên
+		// Các phần code xử lý doanh thu và các thuộc tính khác tôi giữ nguyên
 
 		int sohoadon = 0;
 		int tongsoluongsanpham = 0;
 		int tongtien = 0;
 		String ngaydoanhthucaonhat = "";
 		int tongdoanhthucaonhat = 0;
+
 		List<Object[]> topProducts = spdao.findTop5BestSellingProductsByCategory();
 		model.addAttribute("topProducts", topProducts);
 		List<HoaDon> hoaDonList = hoadondao.thongKeHoaDonTheoNgayTT(tungay, denngay, trangthai);
@@ -171,106 +169,26 @@ public class AdminController {
 
 		}
 
-		int sohoadon_sosanh = 0;
-		int tongsoluongsanpham_sosanh = 0;
-		int tongtien_sosanh = 0;
-		String ngaydoanhthucaonhat_sosanh = "";
-		int tongdoanhthucaonhat_sosanh = 0;
-		List<Object[]> topProducts_sosanh = spdao.findTop5BestSellingProductsByCategory();
-		model.addAttribute("topProducts_sosanh", topProducts_sosanh);
-		List<HoaDon> hoaDonList_sosanh = hoadondao.thongKeHoaDonTheoNgayTT(tungay_sosanh, denngay_sosanh,
-				trangthai_sosanh);
-		List<HoaDonChiTiet> hoaDonDTOList_sosanh = new ArrayList<>();
+		// Thêm những thuộc tính cần thiết vào Model
+		model.addAttribute("tongtien", tongtien);
+		model.addAttribute("ngaydoanhthucaonhat", ngaydoanhthucaonhat);
+		model.addAttribute("tongtiendoanhthucaonhat", tongdoanhthucaonhat);
+		model.addAttribute("sohoadon", sohoadon);
+		model.addAttribute("tongsoluongsanpham", tongsoluongsanpham);
+		model.addAttribute("hoaDonDTOList", hoaDonDTOList);
 
-		for (HoaDon hoaDon_sosanh : hoaDonList_sosanh) {
-			Integer tongSoLuong_sosanh = hoadonchitietdao.tinhTongSoLuongTheoMaHoaDon(hoaDon_sosanh.getMaHoaDon());
-
-			// Tạo một đối tượng HoaDonDTO để lưu thông tin hóa đơn và tổng số lượng
-			HoaDonChiTiet hoaDonDTO_sosanh = new HoaDonChiTiet();
-			hoaDonDTO_sosanh.setHoaDon(hoaDon_sosanh);
-			hoaDonDTO_sosanh.setSoLuong(tongSoLuong_sosanh);
-
-			// Thêm hoaDonDTO vào danh sách hoaDonDTOList
-			hoaDonDTOList_sosanh.add(hoaDonDTO_sosanh);
-			sohoadon_sosanh++;
-			tongsoluongsanpham_sosanh += tongSoLuong_sosanh;
-			tongtien_sosanh += hoaDon_sosanh.getTongTien();
-			if (hoaDon_sosanh.getTongTien() > tongdoanhthucaonhat_sosanh) {
-				tongdoanhthucaonhat_sosanh = hoaDon_sosanh.getTongTien();
-				ngaydoanhthucaonhat_sosanh = hoaDon_sosanh.getNgayDatHang();
-			}
-
-		}
-
-	    // Thêm những thuộc tính cần thiết vào Model
-	    model.addAttribute("tongtien", tongtien);
-	    model.addAttribute("ngaydoanhthucaonhat", ngaydoanhthucaonhat);
-	    model.addAttribute("tongtiendoanhthucaonhat", tongdoanhthucaonhat);
-	    model.addAttribute("sohoadon", sohoadon);
-	    model.addAttribute("tongsoluongsanpham", tongsoluongsanpham);
-	    model.addAttribute("hoaDonDTOList", hoaDonDTOList);
-	    model.addAttribute("tongtien_sosanh", tongtien_sosanh);
-	    model.addAttribute("ngaydoanhthucaonhat_sosanh", ngaydoanhthucaonhat_sosanh);
-	    model.addAttribute("tongtiendoanhthucaonhat_sosanh", tongdoanhthucaonhat_sosanh);
-	    model.addAttribute("sohoadon_sosanh", sohoadon_sosanh);
-	    model.addAttribute("tongsoluongsanpham_sosanh", tongsoluongsanpham_sosanh);
-	    model.addAttribute("hoaDonDTOList_sosanh", hoaDonDTOList_sosanh);
-	    model.addAttribute("chontrangthai", trangthai);
-	    model.addAttribute("chontrangthai_sosanh", trangthai_sosanh);
-
-	    if (tongsoluongsanpham > tongsoluongsanpham_sosanh) {
-	        int tanggiamsanpham = tongsoluongsanpham - tongsoluongsanpham_sosanh;
-	        model.addAttribute("tanggiamsanpham", "Tăng " + tanggiamsanpham);
-	    } else if (tongsoluongsanpham < tongsoluongsanpham_sosanh) {
-	        int tanggiamsanpham = tongsoluongsanpham_sosanh - tongsoluongsanpham;
-	        model.addAttribute("tanggiamsanpham", "Giảm " + tanggiamsanpham);
-	    }
-
-	    return "admin/thongke";
-	}
-	
-//	@ModelAttribute("trangthai_sosanh")
-//	public Map<String, String> getTrangThaiSoSanh() {
-//	    Map<String, String> map = new HashMap<>();
-//	    map.put("wait", "Chờ xét duyệt");
-//	    map.put("approved", "Đã xét duyệt");
-//	    map.put("cancel", "Xét duyệt thất bại");
-//	    map.put("delivery", "Đang giao hàng");
-//	    map.put("delivered", "Đã giao hàng");
-//	    return map;
-//	}
-//
-//	@ModelAttribute("trangthai")
-//	public Map<String, String> getTrangThai() {
-//	    Map<String, String> map = new HashMap<>();
-//	    map.put("wait", "Chờ xét duyệt");
-//	    map.put("approved", "Đã xét duyệt");
-//	    map.put("cancel", "Xét duyệt thất bại");
-//	    map.put("delivery", "Đang giao hàng");
-//	    map.put("delivered", "Đã giao hàng");
-//	    return map;
-//	}
-
-	@ModelAttribute("trangthai_sosanh")
-	public List<String> getTrangThaiSoSanh() {
-		List<String> trangthai_sosanh = new ArrayList<>();
-	    trangthai_sosanh.add("Chờ xét duyệt");
-	    trangthai_sosanh.add("Đã xét duyệt");
-	    trangthai_sosanh.add("Xét duyệt thất bại");
-	    trangthai_sosanh.add("Đang giao hàng");
-	    trangthai_sosanh.add("Đã giao hàng");
-	    return trangthai_sosanh;
+		return "admin/thongke";
 	}
 
 	@ModelAttribute("trangthai")
 	public List<String> getTrangThai() {
 		List<String> trangthai = new ArrayList<>();
-	    trangthai.add("Chờ xét duyệt");
-	    trangthai.add("Đã xét duyệt");
-	    trangthai.add("Xét duyệt thất bại");
-	    trangthai.add("Đang giao hàng");
-	    trangthai.add("Đã giao hàng");
-	    return trangthai;
+		trangthai.add("Chờ xét duyệt");
+		trangthai.add("Đã xét duyệt");
+		trangthai.add("Xét duyệt thất bại");
+		trangthai.add("Đang giao hàng");
+		trangthai.add("Đã giao hàng");
+		return trangthai;
 	}
 
 }
